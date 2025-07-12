50a14fa6e4f16035038dcd6bf1a6df83ac0604d0
Yash Bhatiya <82516328+yashbhatiya@users.noreply.github.com>
2025-07-12 16:10:49 +0530

    Added Resume

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -17,21 +17,27 @@ const HeroSection = () => {
 
   return (
     <section className="min-h-screen flex flex-col justify-center px-6 md:px-12 lg:px-24">
-      <div className="max-w-5xl mx-auto">
+      <div className=" mx-4">
+        <div className="flex items-center gap-3 mb-4">
+          <h2 className="text-2xl md:text-3xl animate-slide-down opacity-0" style={{ animationDelay: '2.6s' }}>
+            ML & Web Developer
+          </h2>
 
+          <button
+            onClick={() => alert('Button Clicked')}
+            className="px-4 py-2  bg-black text-white rounded hover:bg-black transition" style={{ animationDelay: '2.6s' }}
+          >
+            Download CV
+          </button>
+        </div>
 
-
-        <h2 className="text-2xl md:text-3xl mb-6 animate-slide-down opacity-0" style={{ animationDelay: '0.2s' }}>
-          ML & Web Developer
-        </h2>
-
-        <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold mb-8 animate-slide-down opacity-0" style={{ animationDelay: '0.4s' }}>
+        <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold mb-8 animate-slide-down opacity-0" style={{ animationDelay: '2.8s' }}>
           <SplitText className="leading-tight">
-            Hi, I'm Yash, a developer passionate about creating imapctful digital experiences
+            Hi, I'm Yash, a developer passionate about creating imapactful digital experiences
           </SplitText>
         </h1>
 
-        <p className="text-xl md:text-2xl max-w-2xl animate-slide-down opacity-0" style={{ animationDelay: '0.6s' }}>
+        <p className="text-xl md:text-2xl max-w-2xl animate-slide-down opacity-0" style={{ animationDelay: '3.0s' }}>
           I design and develop websites, interfaces, and digital experiences that are beautiful, functional, and focused on user experience.
         </p>
       </div>
