8c0dd9e1740a6064c7aa5eb8274a061fd3a7a3d9
Yash Bhatiya <82516328+yashbhatiya@users.noreply.github.com>
2025-07-12 16:14:25 +0530

    Delayed animation and added png to training part

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -19,25 +19,25 @@ const HeroSection = () => {
     <section className="min-h-screen flex flex-col justify-center px-6 md:px-12 lg:px-24">
       <div className=" mx-4">
         <div className="flex items-center gap-3 mb-4">
-          <h2 className="text-2xl md:text-3xl animate-slide-down opacity-0" style={{ animationDelay: '2.6s' }}>
+          <h2 className="text-2xl md:text-3xl animate-slide-down opacity-0" style={{ animationDelay: '2.8s' }}>
             ML & Web Developer
           </h2>
 
           <button
             onClick={() => alert('Button Clicked')}
-            className="px-4 py-2  bg-black text-white rounded hover:bg-black transition" style={{ animationDelay: '2.6s' }}
+            className="px-4 py-2  bg-black text-white rounded hover:bg-black transition" style={{ animationDelay: '2.8s' }}
           >
             Download CV
           </button>
         </div>
 
-        <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold mb-8 animate-slide-down opacity-0" style={{ animationDelay: '2.8s' }}>
+        <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold mb-8 animate-slide-down opacity-0" style={{ animationDelay: '3.0s' }}>
           <SplitText className="leading-tight">
             Hi, I'm Yash, a developer passionate about creating imapactful digital experiences
           </SplitText>
         </h1>
 
-        <p className="text-xl md:text-2xl max-w-2xl animate-slide-down opacity-0" style={{ animationDelay: '3.0s' }}>
+        <p className="text-xl md:text-2xl max-w-2xl animate-slide-down opacity-0" style={{ animationDelay: '3.2s' }}>
           I design and develop websites, interfaces, and digital experiences that are beautiful, functional, and focused on user experience.
         </p>
       </div>
